2a5ee2b59b88f5bce87597c16666f264edf07459
Swapnil Desai <swapnil.desai@contino.io>
2021-04-25 19:54:43 +1000

    added time

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,75 @@
+/* eslint-disable no-undef */
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-  console.log('Count has been updated', count);
+// Elements
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#sendLocation');
+const $messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+
+socket.on('message', (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a'),
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#increment').addEventListener('click', () => {
-  console.log('clicked event');
-  socket.emit('increment');
+socket.on('locationMessage', (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a'),
+  });
+  $messages.insertAdjacentHTML('beforeend', html);
+});
+
+$messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //disable
+
+  $messageFormButton.setAttribute('disabled', 'disabled');
+
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message, (error) => {
+    // enable
+    $messageFormButton.removeAttribute('disabled');
+    $messageFormInput.value = '';
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message delivered');
+  });
+});
+
+$sendLocationButton.addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  $sendLocationButton.setAttribute('disabled', 'disabled');
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        $sendLocationButton.removeAttribute('disabled');
+        console.log('Location shared!');
+      }
+    );
+  });
 });
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,17 +14,33 @@ const publicPathDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicPathDirectory));
 
-let count = 0;
-
 io.on('connection', (socket) => {
   console.log('New websocket connection');
 
-  socket.emit('countUpdated', count);
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined'));
 
-  socket.on('increment', () => {
-    count++;
-    // socket.emit('countUpdated', count);
-    io.emit('countUpdated', count);
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed');
+    }
+
+    io.emit('message', generateMessage(message));
+    callback();
+  });
+
+  socket.on('sendLocation', (cords, callback) => {
+    io.emit(
+      'locationMessage',
+      generateLocationMessage(`https://google.com/maps?q=${cords.latitude},${cords.longitude}`)
+    );
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', generateMessage('A user has left'));
   });
 });
 
